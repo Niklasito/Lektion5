d379e03be2276d12054bab1b9dc547f8903c808f
Niklasito <niklasandersson.gbg@gmail.com>
2023-04-12 13:01:56 +0200

    Add project files.

---
+++ WebApp/Controllers/AccountController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebApp.Services;
+using WebApp.ViewModels;
+
+namespace WebApp.Controllers;
+
+public class AccountController : Controller
+{
+
+    private readonly AuthService _auth;
+
+    public AccountController(AuthService auth)
+    {
+        _auth = auth;
+    }
+
+
+    [Authorize]
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult SignUp()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> SignUp(UserSignUpViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+
+            if(await _auth.SignUpAsync(model))
+                return RedirectToAction("Index");
+
+            ModelState.AddModelError("", "A user with the same email-address already exists!"); 
+        }
+        
+        return View(model);
+    }
+}
---
+++ WebApp/Services/AuthService.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Identity;
+using WebApp.Contexts;
+using WebApp.Models.Entities;
+using WebApp.ViewModels;
+
+namespace WebApp.Services;
+
+public class AuthService
+{
+
+    private readonly UserManager<IdentityUser> _userManager;
+    private readonly IdentityContext _identityContext;
+
+    public AuthService(UserManager<IdentityUser> userManager, IdentityContext identityContext)
+    {
+        _userManager = userManager;
+        _identityContext = identityContext;
+    }
+
+    
+
+    public async Task<bool> SignUpAsync(UserSignUpViewModel model)
+    {
+        try
+        {
+            IdentityUser identityUser = model;
+
+            await _userManager.CreateAsync(identityUser, model.Password);
+
+            UserProfileEntity userProfileEntity = model;
+            userProfileEntity.UserId = identityUser.Id;
+
+            _identityContext.UserProfiles.Add(model);
+            await _identityContext.SaveChangesAsync();
+
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+      }
+
+
+
+}
